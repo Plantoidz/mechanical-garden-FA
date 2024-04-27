ea013c07879cf862ce100cd68652f30fdebc9eb4
Giulio Trichilo <giulio.trichilo.pay@gmail.com>
2024-04-27 23:38:10 +0200

    update configure characters py

--- config/scripts/configure_characters.py
+++ config/scripts/configure_characters.py
@@ -7,9 +7,9 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-import utils.serial_utils
+#import utils.serial_utils
 import serial.tools.list_ports
-from utils.serial_utils import send_to_arduino
+# from utils.serial_utils import send_to_arduino
 
 class CharacterConfigurator:
     def __init__(self, source_file, destination_file):
