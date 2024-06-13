49cd69a53c677bd0bcf0847fa7a61e1cb5140ec2
yaoe <pdefilippi@gmail.com>
2024-06-13 15:18:12 +0200

    changed personalities

--- interaction_modes/interaction.py
+++ interaction_modes/interaction.py
@@ -24,7 +24,7 @@ class PlantoidInteraction:
         self.select_next_speaker = selection_function
         self.last_speaker_idx = 0
         self.current_speaker_idx = 0
-        self.humanness = 0.5
+        self.humanness = 0.7
         self.agent_interrupted = False
         self.interaction_timestamp = datetime.now()
 
